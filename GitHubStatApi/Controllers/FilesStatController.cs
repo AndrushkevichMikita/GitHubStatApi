1b4ae405f37b6e466eddcf8759caf1f32352f880
Mikita Andrushkevich <Mikita.Andrushkevich@ventionteams.com>
2025-01-20 10:40:16 +0200

    GitHubStatApi. Added FilesStatController

---
+++ GitHubStatApi/Controllers/FilesStatController.cs
@@ -0,0 +1,29 @@
+﻿using GitHubStatApi.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GitHubStatApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilesStatController : ControllerBase
+    {
+        private readonly IGitHubService _gitHubService;
+
+        private readonly IFileContentAnalyzerService _fileProcessor;
+
+        public FilesStatController(IGitHubService gitHubService, IFileContentAnalyzerService fileProcessor)
+        {
+            _gitHubService = gitHubService;
+            _fileProcessor = fileProcessor;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetLetterFrequencies()
+        {
+            var filesContent = await _gitHubService.GetContentOfTSAndJSFiles(HttpContext.RequestAborted);
+            var frequencies = _fileProcessor.CalculateLetterFrequencies(filesContent);
+
+            return Ok(frequencies);
+        }
+    }
+}
